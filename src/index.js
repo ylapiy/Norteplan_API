f52f08112a53daa1d0e6933a671cf12c33149272
ylapiy <ygorgivagooo@gmail.com>
2025-07-16 18:05:07 -0300

    adicionando o put

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const fastify = Fastify({
 });
 
 fastify.register(require("@fastify/postgres"), {
-  connectionString: "",
+  connectionString: "postgresql://neondb_owner:npg_oY2EBJrcb5AR@ep-billowing-recipe-ad8vh7sp-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require",
 });
 
 fastify.listen({ port: 3000 }, function (error, address) {
@@ -73,5 +73,78 @@ fastify.post("/criaprojeto", async (req, res) => {
     res.status(500).send(error);
   }
 });
+
+fastify.put("/editaprojeto", async (req, res) => {
+
+try{
+
+    const client = await fastify.pg.connect();
+
+    const {
+      engenheiro,
+      municipio,
+      objeto,
+      prioridade,
+      inicio,
+      fim,
+      financiamento,
+      vencimento_convenio,
+      clasula_suspensiva,
+      observações,
+    } = req.body;
+
+    const query = `
+      UPDATE projetos SET 
+        engenheiro = $2, 
+        municipio = $3, 
+        objeto = $4, 
+        prioridade = $5, 
+        inicio = $6, 
+        fim = $7,
+        financiamento = $8, 
+        vencimento_convenio = $9, 
+        clasula_suspensiva = $10 
+        observações = $11,
+        WHERE id = $1`;
+
+    const values = [
+      Id,
+      engenheiro,
+      municipio,
+      objeto,
+      prioridade,
+      inicio,
+      fim,
+      financiamento,
+      vencimento_convenio,
+      clasula_suspensiva,
+      observações
+    ];
+
+}catch(error){
+
+
+
+
+}
+
+})
+
+
+fastify.get("/getservicos", async (req, res) =>{
+    
+    try {
+    const client = await fastify.pg.connect(); 
+    const result = await client.query("SELECT * FROM serviços");
+    client.release();
+    res.send(result.rows); 
+
+  } catch (error) {
+    res.status(500).send(error);
+  }
+
+
+
+})
 
 
