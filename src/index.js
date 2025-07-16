afa51131d27d3757b0ef23089422ca25c0f6a529
ylapiy <ygorgivagooo@gmail.com>
2025-07-16 17:19:57 -0300

    coneção com o banco de dados , get e post funcionando

--- src/index.js
+++ src/index.js
@@ -1,27 +1,77 @@
 const Fastify = require("fastify");
-const { Connection } = require("pg");
 
 const fastify = Fastify({
-    logger: false
+  logger: false,
 });
 
-fastify.register(require("@fastify/postgres"), {ConnectionString:'postgresql://neondb_owner:npg_oY2EBJrcb5AR@ep-billowing-recipe-ad8vh7sp-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require', ssl: {rejectUnauthorized: false}
-}
-)
-
-fastify.get("/projetos", function(req, res){
-    res.send({hello : "world"});
-
-
+fastify.register(require("@fastify/postgres"), {
+  connectionString: "",
 });
 
-fastify.listen({ port:3000}, function(error,address){
-    
-    if(error){
-        console.log(erro);
-        process.exit(1)
-    }
-    
-    console.log("Servidor rodando", address);
+fastify.listen({ port: 3000 }, function (error, address) {
+  if (error) {
+    console.log(error); 
+    process.exit(1);
+  }
 
-})
+  console.log("Servidor rodando em:", address);
+});
+
+fastify.get("/getprojetos", async (req, res) => {
+  try {
+    const client = await fastify.pg.connect(); 
+    const result = await client.query("SELECT * FROM projetos");
+    client.release();
+    res.send(result.rows); 
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+fastify.post("/criaprojeto", async (req, res) => {
+  try {
+    const client = await fastify.pg.connect();
+
+    const {
+      engenheiro,
+      municipio,
+      objeto,
+      prioridade,
+      inicio,
+      fim,
+      financiamento,
+      vencimento_convenio,
+      clasula_suspensiva,
+      observações,
+    } = req.body;
+
+    const query = `
+      INSERT INTO projetos (
+        engenheiro, municipio, objeto, prioridade, inicio, fim,
+        financiamento, vencimento_convenio, clasula_suspensiva, observações
+      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+    `;
+
+    const values = [
+      engenheiro,
+      municipio,
+      objeto,
+      prioridade,
+      inicio,
+      fim,
+      financiamento,
+      vencimento_convenio,
+      clasula_suspensiva,
+      observações
+    ];
+
+    await client.query(query, values);
+    client.release();
+
+    res.send({ success: true, message: "Olha o banco" });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+
