6d4c2d9d24484801ac385061c8e595fe3e8411c5
ylapiy <163143777+ylapiy@users.noreply.github.com>
2025-09-11 03:09:53 -0300

    Update plugins.js

--- src/plugins.js
+++ src/plugins.js
@@ -18,24 +18,10 @@ fastify.register(require("@fastify/multipart"), {
 
 fastify.register(require("@fastify/postgres"), {
   connectionString:
-    "postgresql://neondb_owner:npg_oY2EBJrcb5AR@ep-billowing-recipe-ad8vh7sp-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require",
+    process.env.DATABASE_URL,
 });
 
-const credentials = {
-  web: {
-    client_id:
-      "508528174189-upsji6h2eip0dv4uqej1da7119ks66kr.apps.googleusercontent.com",
-    project_id: "norteplandriveconcection",
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_secret: "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",
-    redirect_uris: [
-      "http://localhost:3000/oauth2callback",
-      "https://norteplanapi-production.up.railway.app/oauth2callback",
-    ],
-  },
-};
+const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS_JSON)
 
 const CLIENT_ID = credentials.web.client_id;
 const CLIENT_SECRET = credentials.web.client_secret;
